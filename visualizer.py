26ce1334a33037ca9e968634ef68bb9818ee06f4
DenisMiX <45606051+DenisMihailapov@users.noreply.github.com>
2021-07-20 22:16:50 +0700

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -7,13 +7,14 @@ import numpy as np
 
 class Visualizer:
     
-    def __init__(self, model, dataset):
+    def __init__(self, model, dataset, device='cuda'):
         self._model = model
         self._dataset = dataset
+        self._device = device
 
 
     def _show(self, imgs):
-        if not isinstance(imgs, list):
+        if not isinstance(imgs, list): 
             imgs = [imgs]
         l = len(imgs)    
         fix, axs = plt.subplots(ncols=l, squeeze=False, figsize=(5*l,5*l))
@@ -33,16 +34,15 @@ class Visualizer:
         num_examples = len(indexes); 
         
         img, mask = self._dataset[indexes[0]].values(); num_examples-=1; 
-        
-        print(img.type(), mask.type())
-        
+
         if with_model: 
             self._model.eval()
-            pred = self._model(img.unsqueeze(0).cuda())[0]
+            pred = self._model(img.unsqueeze(0).to(self._device))[0][0]
             
             print('    ','Image','\t\t\t\t','True Mask','\t\t\t','Predict')
-            print('    ',img.shape,'\t\t', mask.shape,'\t', pred.shape)
-            print(img.type(), mask.type(), pred.type())
+            print('    ',img.shape, '\t\t', mask.shape, '\t', pred.shape)
+            print('    ',img.type(),'\t\t', mask.type(),'\t', pred.type())
+
             self._show([img, mask, pred]) 
         else:
             print('    ','Image','\t\t\t\t','Mask')
@@ -53,6 +53,6 @@ class Visualizer:
         for i in range(1, num_examples):
             img, mask = self._dataset[indexes[i]].values()
             if with_model: 
-                pred = self._model(img.unsqueeze(0).cuda())[0]
+                pred = self._model(img.unsqueeze(0).to(self._device))[0][0]
                 self._show([img, mask, pred])
             else: self._show([img, mask])        
